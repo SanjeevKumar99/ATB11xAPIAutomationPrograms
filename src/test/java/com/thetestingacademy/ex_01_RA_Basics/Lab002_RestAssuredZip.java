1aad74cc885812f5f390d03a2646c2828881c86e
sanjeev <sanjeevskk99@gmail.com>
2025-05-08 11:24:15 +0530

    commited -05/08

---
+++ src/test/java/com/thetestingacademy/ex_01_RA_Basics/Lab002_RestAssuredZip.java
@@ -0,0 +1,4 @@
+package com.thetestingacademy.ex_01_RA_Basics;
+
+public class Lab002_RestAssuredZip {
+}
---
+++ src/test/java/com/thetestingacademy/ex_03_TestNG_AllureReport/Lab004_APITesting_TestCase.java
@@ -0,0 +1,4 @@
+package com.thetestingacademy.ex_03_TestNG_AllureReport;
+
+public class Lab004_APITesting_TestCase {
+}
